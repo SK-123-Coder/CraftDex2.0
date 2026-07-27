32bea16bf9d84e1b71e7e6616d702a8892d7c751
SK_Coder <sarangkhandateofficial@gmail.com>
2026-07-27 15:18:25 +0530

    1.6

--- client/src/tabs/ResourcesPage.jsx
+++ client/src/tabs/ResourcesPage.jsx
@@ -269,7 +269,7 @@ function ResourcesPage(){
                     >
                     {loading ? (
                         <div className="flex items-center justify-center h-64">
-                        <p className="text-gray-400">Loading...</p>
+                        <p className="text-gray-400 font-semibold text-xl">Loading...</p>
                         </div>
                     ) : (
                         <MarkdownViewer content={markdown} />
