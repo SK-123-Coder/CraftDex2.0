eef1bcf53f5d6d45654883053ea5f8f02f491d1a
SK_Coder <sarangkhandateofficial@gmail.com>
2026-07-28 15:39:42 +0530

    2.3

--- server/App.js
+++ server/App.js
@@ -25,7 +25,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
   origin: [
-    "http://localhost:5173"  //"https://craftdex.in" // 
+    "http://localhost:5173",
+    "http://localhost:4000"  //"https://craftdex.in" // 
   ],
   credentials: true,
 }));
@@ -40,7 +41,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173"  // "https://craftdex.in"  // 
+      "http://localhost:5173",
+      "http://localhost:4000"  // "https://craftdex.in"  // 
     ],
     credentials: true,
   },
